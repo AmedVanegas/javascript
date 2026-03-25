6a957dac9cd51c70614e246722de6ad7b83af1ed
Amed Vanegas <amedvanegas368@gmail.com>
2026-03-25 18:18:19 -0500

    tipos de datos

---
+++ 0-2data-types.js
@@ -0,0 +1,12 @@
+// primitivos, string
+
+"maria"
+23
+14.8
+true
+false
+
+// de referencia
+
+console.log(34)
+console.log("34")
---
+++ 0.2variables.js
@@ -0,0 +1,19 @@
+// let, const, var
+
+let saludo = "hola";
+
+let name1 = "maria";
+
+
+console.log(saludo , name1 )
+
+name1 = "pedro"  // se puede reescribir
+
+console.log(saludo , name1)
+
+const name2 = "Jesus" // no se puede reescribir
+
+console.log(name2)
+
+
+
