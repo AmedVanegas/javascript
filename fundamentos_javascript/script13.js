11c2591328e0d87b815b55b2dc03636a2e034597
Amed Vanegas <amedvanegas368@gmail.com>
2026-03-26 21:24:05 -0500

    kjdasnkjdaskjd

--- fundamentos_javascript/script13.js
+++ fundamentos_javascript/script13.js
@@ -9,13 +9,14 @@ let valorCena = Number(prompt("dime el costo de la cena "));
 let nPersonas = Number(prompt("dime el numero de personas "));
 let nombreMas = prompt("quien comio mas?");
 let nombreMenos = prompt("quien comio menos?");
-let vPersona = valorCena / (nPersonas + 0.5);
+let vPersona = valorCena / (nPersonas + 0.2);
 let mas = vPersona*2;
-let mitad = Math.trunc(vPersona*0.5);
+let medio = vPersona*0.5;
+let mitad = Math.trunc(vPersona*0.8);;
 
 
 
 
 
-alert(`El valor de la cena por cada persona es ${vPersona.toFixed(2)} pero ${nombreMas} paga ${mas.toFixed(2)} y ${nombreMenos} paga ${mitad.toFixed(2)}`)
+alert(`El valor de la cena por cada persona es ${vPersona.toFixed(0)} pero ${nombreMas} paga ${mas.toFixed(0)} y ${nombreMenos} paga ${mitad.toFixed(0)}  sara come ${medio.toFixed(0)}`)
 
