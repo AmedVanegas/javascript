4fe084da12f46d048d425e98fc4192a96cfb8af3
Amed Vanegas <amedvanegas368@gmail.com>
2026-04-07 20:26:00 -0500

    tablas

---
+++ clase js/script8.js
@@ -0,0 +1,22 @@
+ let num = Number(prompt(` ¿ De que numero quieres tu tabla ? `))
+
+ let largo = Number(prompt( ` ¿ Hasta que numero quieres tu tabla ?`))
+
+ if ( num > 11 , num < 1){
+
+    alert("Ingrese un numero valido")
+
+
+
+ } else{
+
+    for (i = 1 ; i < ( largo + 1 ) ; i++){
+
+    console.log(`${num} * ${i}  = ${i * num}`)
+
+
+}
+
+
+
+}
