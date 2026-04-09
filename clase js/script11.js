71037646cdeb1c16b5feceb13eb8afcc792958f0
Amed Vanegas <amedvanegas368@gmail.com>
2026-04-08 21:18:24 -0500

    aaaa

--- clase js/script11.js
+++ clase js/script11.js
@@ -1,33 +1,20 @@
 const estudiantes = [
-  { nombre: "Juan", apellido: "Pérez", notaFinal: 4.5 },
-  { nombre: "María", apellido: "Gómez", notaFinal: 3.8 },
-  { nombre: "Carlos", apellido: "Rodríguez", notaFinal: 2.0 },
-  { nombre: "Ana", apellido: "Martínez", notaFinal: 4.9 },
-  { nombre: "Luis", apellido: "Fernández", notaFinal: 3.5 }
+  { nombre: "Juan", apellido: "Pérez", notaFinal: [2.0, 4.5, 5.0, 3.5] },
+  { nombre: "María", apellido: "Gómez", notaFinal: [2.0, 4.5, 5.0, 3.5] },
+  { nombre: "Carlos", apellido: "Rodríguez", notaFinal: [2.0, 4.5, 5.0, 3.5] },
+  { nombre: "Ana", apellido: "Martínez", notaFinal: [] },
+  { nombre: "Luis", apellido: "Fernández", notaFinal: []}
 ];
 
 for(i = 0 ; i < estudiantes.length ; i ++ ){
 
     let note = estudiantes[i].notaFinal
     let name = estudiantes[i].nombre
-    let estado = 0
+    let estado = ""
 
-    if ( estudiantes[i].notaFinal < 3.0){
-
-        estado = "perdio"
-
-
-
-    }else if (estudiantes[i].notaFinal < 4.0){
-
-        estado = "nivela"
-
-    }else{
-
-        estado = "paso"
-
-
-    }
+    if ( estudiantes[i].notaFinal < 3.0) estado = "perdio"
+    else if (estudiantes[i].notaFinal < 4.0) estado = "nivela"
+    else  estado = "paso" 
 
     console.log(`El estudiante ${name} ${estado} la materia con ${note}`)
 
