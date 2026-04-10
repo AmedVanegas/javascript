4c7b9f7547406548ffc5700e578b8818e7a7010f
Amed Vanegas <amedvanegas368@gmail.com>
2026-04-09 20:03:15 -0500

    biciesto

---
+++ clase js/script15.js
@@ -0,0 +1,12 @@
+let año = Number(prompt("dime un año"))
+
+
+if((año % 4 == 0 && año % 100 !== 0 ) || (año % 400) ){
+
+    alert(`el año ${año} es biciesto`)
+
+
+}else{
+
+    alert(`el año ${año} no es biciesto`)
+}
