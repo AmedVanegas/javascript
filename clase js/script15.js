23bac6626475ad1252248159b380fa1b80d49156
Amed Vanegas <amedvanegas368@gmail.com>
2026-04-09 20:27:40 -0500

    array biciesto

--- clase js/script15.js
+++ clase js/script15.js
@@ -1,12 +1,32 @@
-let año = Number(prompt("dime un año"))
+
+let data = [2024, 100, 3000, 2100]
+let biciesto = ""
 
 
-if( ( año % 4 == 0 && año % 100 !== 0 ) || (año % 400 == 0 ) ){
+for (i = 0; i < data.length; i++) {
 
-    alert(`el año ${año} es biciesto`)
+    if ((data[i] % 4 == 0 && data[i] % 100 !== 0) || (data[i] % 400 == 0)) {
+
+        alert(`el año ${data[i]} es biciesto`)
 
 
-}else{
+    } else {
 
-    alert(`el año ${año} no es biciesto`)
+        alert(`el año ${data[i]} no es biciesto`)
+    }
+
+
+
 }
+
+
+
+// if ((año % 4 == 0 && año % 100 !== 0) || (año % 400 == 0)) {
+
+//     alert(`el año ${año} es biciesto`)
+
+
+// } else {
+
+//     alert(`el año ${año} no es biciesto`)
+// }
